17e6700a046dc12d55f7e5a2aa980d0f953870c1
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-06-03 20:02:14 +0900

    세팅

---
+++ app/global.php
@@ -0,0 +1,9 @@
+<?php
+// 리포지터리
+$App__memberRepository = new APP__MemberRepository();
+$App__articleRepository = new APP__ArticleRepository();
+
+// 서비스 전역변수
+$App__memberService = new APP__MemberService();
+$App__articleService = new APP__ArticleService();
+
